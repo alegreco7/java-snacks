459998bc62cabb37617e206f5706aab17a34df96
alegreco7 <alexgreco2002@gmail.com>
2024-05-16 14:21:25 +0200

    snacks 1: finito

--- src/Snack1.java
+++ src/Snack1.java
@@ -3,8 +3,20 @@ Snack1
 Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 */
 
+import java.util.Scanner;
+
 public class Snack1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci un numero");
+        int numero = scanner.nextInt();
 
+        if (numero % 2 == 0) {
+            System.out.println("Il numero inserito è pari");
+        } else {
+            System.out.println("Il numero inserito è dispari ");
+        }
+
+        scanner.close();
     }
 }
