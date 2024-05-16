56379dfbea5a5575bc9e9121c5e1a9ad33a048ac
alegreco7 <alexgreco2002@gmail.com>
2024-05-16 14:44:20 +0200

    snacks 3: finito

--- src/Snack3.java
+++ src/Snack3.java
@@ -5,6 +5,22 @@ Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in 
 
 public class Snack3 {
     public static void main(String[] args) {
+        // Array di numeri interi
+        int[] numeri = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 
+        // Variabile per memorizzare la somma degli elementi in posizione dispari
+        int sommaDispari = 0;
+
+        // Itera sugli elementi dell'array
+        for (int i = 0; i < numeri.length; i++) {
+            // Verifica se l'indice è pari
+            if (i % 2 == 0) {
+                // Aggiungi il valore dell'elemento alla somma totale
+                sommaDispari += numeri[i];
+            }
+        }
+
+        // Stampare la somma degli elementi in posizione dispari
+        System.out.println("La somma degli elementi in posizione dispari è: " + sommaDispari);
     }
 }
