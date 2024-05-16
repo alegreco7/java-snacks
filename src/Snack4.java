43679e9bb05cf655e1d3d55798328def3bbe289c
alegreco7 <alexgreco2002@gmail.com>
2024-05-16 14:56:26 +0200

    snacks 3: finito

--- src/Snack4.java
+++ src/Snack4.java
@@ -3,8 +3,33 @@ Snack4
 Data in input una stringa verificare se è palindroma
 */
 
-public class Snack4 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Snack4 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Verifica se una parola è palindroma:");
+        String stringa = input.nextLine();
+
+
+        input.close();
+
+        // Verifica se la stringa è palindroma
+        boolean palindroma = true;
+        for (int i = 0; i < stringa.length() / 2; i++) {
+            if (stringa.charAt(i) != stringa.charAt(stringa.length() - 1 - i)) {
+                palindroma = false;
+                break;
+            }
+        }
+
+        // Stampare il risultato
+        if (palindroma) {
+            System.out.println("La stringa è palindroma.");
+        } else {
+            System.out.println("La stringa non è palindroma.");
+        }
     }
 }
